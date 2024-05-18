c5d304b1def310dd56f009b6c9449f717e77e86c
AlPes <iaalvearp@gmail.com>
2024-05-18 17:59:25 -0500

    Installing packages and setting configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import animations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				custom: ["font-sans", "open sans"],
+				custom: ["font-handwriting", "sans-serif"],
+				custom: ["font-serif", "sans-serif"],
+			},
+		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
