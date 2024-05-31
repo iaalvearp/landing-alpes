7ce94aa578fb744dd7d9713016ac5303adbda23a
Alpes <iaalvearp@gmail.com>
2024-05-31 18:18:20 -0500

    Dark-light preferences done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import animations from '@midudev/tailwind-animations'
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'media',
 	theme: {
 		extend: {
 			fontFamily: {
@@ -13,8 +14,10 @@ export default {
 				twoSans: ['two-sans', 'sans'],
 			},
 			colors: {
-				'primary': '#333',
-				'secondary': '#DDD'
+				'primary': '#DDD',
+				'secondary': '#333',
+				'gold-dark': '#A87B05',
+				'gold-light': '#EFB810',
 			}
 		},
 	},
