1ca8d2d53d141602039327e55c2368a9de1c888e
Alpes <iaalvearp@gmail.com>
2024-06-18 04:49:20 -0500

    Setting projects variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,21 +5,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'media',
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['font-sans', 'sans-serif'],
-				handwriting: ['font-handwriting', 'sans-serif'],
-				serif: ['font-serif', 'serif'],
-				twoSerif: ['two-serif', 'serif'],
-				twoSans: ['two-sans', 'sans'],
-			},
-			colors: {
-				'primary': '#DDD',
-				'secondary': '#333',
-				'gold-dark': '#A87B05',
-				'gold-light': '#EFB810',
-			}
-		},
+		extend: {},
 	},
 	plugins: [animations],
 }
